3bd4af3b0600f3f4066cc41fe9f89f906869587c
Jehoshuah K <musicalbard@gmail.com>
2018-10-13 03:30:14 -0500

    asteroids spawn and wrap and move randomly

--- state0.js
+++ state0.js
@@ -3,7 +3,22 @@ let demo = {},
   centerY = 1500 / 2,
   ship,
   speed = 400,
-  weapon;
+  weapon,
+  spawnAllowed = true,
+  asteroidGroup,
+  asteroidProperties = {
+    startingAsteroids: 4,
+    maxAsteroids: 20,
+    incrementAsteroids: 2,
+  },
+  mainAsteroid = {
+    minVelocity: 50,
+    maxVelocity: 150,
+    minAngularVelocity: 0,
+    maxAngularVelocity: 200,
+    score: 10,
+  },
+  asteroidCount = asteroidProperties.startingAsteroids;
 
 demo.state0 = function() {};
 demo.state0.prototype = {
@@ -16,6 +31,7 @@ demo.state0.prototype = {
     );
     game.load.image('bullet', 'assets/sprites/bullet.png');
     game.load.image('space', 'assets/backgrounds/space.png');
+    game.load.image('asteroid', 'assets/sprites/asteroid.png');
   },
   create: function() {
     game.physics.startSystem(Phaser.Physics.ARCADE);
@@ -48,6 +64,10 @@ demo.state0.prototype = {
     weapon.bulletSpeed = speed;
     weapon.fireRate = 1000;
     weapon.trackSprite(ship, 20, 20, true);
+    asteroidGroup = this.game.add.group();
+    asteroidGroup.enableBody = true;
+    asteroidGroup.physicsBodyType = Phaser.Physics.ARCADE;
+    this.resetAsteroids();
   },
   update: function() {
     if (game.input.keyboard.isDown(Phaser.Keyboard.D)) {
@@ -73,5 +93,61 @@ demo.state0.prototype = {
       weapon.fire();
     }
     game.world.wrap(ship, 0, true);
+    // game.world.wrap(asteroidGroup, 0, true);
+    // asteroidGroup.x = Math.floor(Math.random() * 10);
+    // asteroidGroup.y = Math.floor(Math.random() * 10);
+    asteroidGroup.forEachExists(this.checkBoundaries, this);
+  },
+
+  checkBoundaries: function(sprite) {
+    if (sprite.x < 0) {
+      sprite.x = game.width;
+    } else if (sprite.x > game.width) {
+      sprite.x = 0;
+    }
+    if (sprite.y < 0) {
+      sprite.y = game.height;
+    } else if (sprite.y > game.height) {
+      sprite.y = 0;
+    }
+  },
+
+  createAsteroid: function(x, y, size) {
+    let asteroid = asteroidGroup.create(x, y, 'asteroid');
+    asteroid.anchor.set(0.5, 0.5);
+    asteroid.body.angularVelocity = game.rnd.integerInRange(
+      size.minAngularVelocity,
+      size.maxAngularVelocity
+    );
+    let randomAngle = game.math.degToRad(game.rnd.angle());
+    let randomVelocity = game.rnd.integerInRange(
+      size.minVelocity,
+      size.maxVelocity
+    );
+
+    game.physics.arcade.velocityFromRotation(
+      randomAngle,
+      randomVelocity,
+      asteroid.body.velocity
+    );
+    console.log('asteroid created');
+  },
+
+  resetAsteroids: function() {
+    for (let i = 0; i < asteroidCount; i++) {
+      let side = Math.round(Math.random());
+      let x;
+      let y;
+
+      if (side) {
+        x = Math.round(Math.random());
+        y = Math.round(Math.random() * 2000);
+      } else {
+        x = Math.round(Math.random() * 2000);
+        y = Math.round(Math.random() * 1500);
+      }
+      console.log('inside reset asteroid', mainAsteroid);
+      this.createAsteroid(x, y, mainAsteroid);
+    }
   },
 };
